0e959b78e4b7b04388ae3998212a6562b4e75301
E. Thomaz Rosa <elcio.thomaz@gmail.com>
2023-02-15 16:57:07 -0300

    Adding function based views.

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
-    path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_delete'),
     path('drafts/', views.DraftListView.as_view(), name='draft_list'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment'),
+    path('comment/<int:pk>/approve/', views.approve_comment, name='approve_comment'),
+    path('comment/<int:pk>/remove/', views.remove_comment, name='remove_comment'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
-# from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (TemplateView, ListView,
                                   DetailView, CreateView,
@@ -55,3 +56,46 @@ class DraftListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Post.objects.filter(publication_date__isnull=True).order_by('creation_date')
+
+
+#######################################
+## Functions that require a PK match ##
+#######################################
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
+
+@login_required
+def approve_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def remove_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
+
